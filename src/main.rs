ca91217588e72dca9b82bac5c19efcccce041d2c
theora59 <clem6kiruagon@gmx.fr>
2024-09-29 21:05:21 +0200

    2024-09-29 21:04 : Test du concept d'ECS

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use bevy::prelude::*;
+
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+    .add_plugins(DefaultPlugins)
+    .add_systems(Startup, 
+        add_people
+    )
+    .add_systems(Update, 
+        greet_people
+    )
+    .run();
+
+}
+
+#[derive(Component)]
+struct Person;
+
+
+
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Elaina Proctor".to_string())));
+    commands.spawn((Person, Name("Renzo Hume".to_string())));
+    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+}
+
+
+fn hello_world(time: Res<Time>) {
+    println!("Hello, Bevy!");
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("hello {}!", name.0);
+    }
 }
