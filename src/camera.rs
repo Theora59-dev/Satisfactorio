daed75fa06a944df0c2c4292bf8ccccbb5d4f2eb
theora59 <clem6kiruagon@gmx.fr>
2024-10-20 19:33:33 +0200

    Nettoyage du code 2

--- src/camera.rs
+++ src/camera.rs
@@ -7,7 +7,7 @@ pub struct CameraRotRelative(pub f32);
 pub fn move_camera(mut mouse_input: EventReader<MouseMotion>, mut query: Query<&mut Transform, With<Camera>>, mut camera_rot_relative: ResMut<CameraRotRelative>) {
     let sensitivity = 0.00048828125;
     let pi_sur_deux = 1.570796326794897;
-    let mut rot_x = 0.00;
+    let mut rot_x: f32;
     for mut transform in query.iter_mut() {
         // Orientation Caméra
         for ev in mouse_input.read() {
