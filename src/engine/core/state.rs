5521b372c0364b05fb81b09d0a729740d6ad1250
Theora59-dev <clem6kiruagon@gmx.fr>
2026-03-08 21:53:15 +0100

    correction

--- src/engine/core/state.rs
+++ src/engine/core/state.rs
@@ -251,51 +251,6 @@ impl State {
             cache: None,
         });
 
-<<<<<<< HEAD
-        let gizmo_render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-            label: Some("Render Pipeline"),
-            layout: Some(&render_pipeline_layout),
-            vertex: wgpu::VertexState {
-                module: &shader,
-                entry_point: Some("vs_main"),
-                buffers: &[Vertex::buffer_layout()],
-                compilation_options: wgpu::PipelineCompilationOptions::default(),
-            },
-            fragment: Some(wgpu::FragmentState {
-                module: &shader,
-                entry_point: Some("fs_main"),
-                targets: &[Some(wgpu::ColorTargetState {
-                    // 4.
-                    format: config.format,
-                    blend: Some(wgpu::BlendState::REPLACE),
-                    write_mask: wgpu::ColorWrites::ALL,
-                })],
-                compilation_options: wgpu::PipelineCompilationOptions::default(),
-            }),
-            primitive: wgpu::PrimitiveState {
-                topology: wgpu::PrimitiveTopology::LineList,
-                strip_index_format: None,
-                front_face: wgpu::FrontFace::Ccw,
-                cull_mode: None,
-                // cull_mode: Some(wgpu::Face::Back),
-                // Setting this to anything other than Fill requires Features::NON_FILL_POLYGON_MODE
-                polygon_mode: wgpu::PolygonMode::Fill,
-                // Requires Features::DEPTH_CLIP_CONTROL
-                unclipped_depth: false,
-                // Requires Features::CONSERVATIVE_RASTERIZATION
-                conservative: false,
-            },
-            depth_stencil: None,
-            multisample: wgpu::MultisampleState {
-                count: 1,
-                mask: !0,
-                alpha_to_coverage_enabled: false,
-            },
-            multiview_mask: None,
-            cache: None,
-        });
-        
-=======
         let gizmo_render_pipeline =
             device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
                 label: Some("Render Pipeline"),
@@ -340,13 +295,10 @@ impl State {
                 cache: None,
             });
 
-        let mut world = World::new();
->>>>>>> 408a460 (Ajout des movement joueurs + cam)
         let player = Player::new();
 
-        let world_start = Instant::now();
-
         let mut world = World::new();
+        let world_start = Instant::now();
 
         let [min_x, max_x, min_y, max_y, min_z, max_z] = player.get_rendered_chunk_range();
 
@@ -359,7 +311,10 @@ impl State {
             }
         }
 
-        println!("Time to make the world: {:.3}ms.", world_start.elapsed().as_micros().to_f64().unwrap() / 1_000.0);
+        println!(
+            "Time to make the world: {:.3}ms.",
+            world_start.elapsed().as_micros().to_f64().unwrap() / 1_000.0
+        );
 
         let start = Instant::now();
 
